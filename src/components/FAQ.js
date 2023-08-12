025f65ab95d964ff74f293ae17169c4f696ad291
Pratik Vairat <pratikvairat30@gmail.com>
2023-08-12 21:15:28 +0530

    FAQ Component Upadte

---
+++ src/components/FAQ.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import "./css/FAQ.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { FAQQuestion } from '../data/FAQQuestion';
+
+function FAQ() {
+    const [accordion, setAccordion] = useState(-1);
+    function toggleAccordion(index) {
+        if(accordion===index){
+            setAccordion(-1);
+        }else{
+            setAccordion(index);
+        }
+    }
+    return (
+        <React.Fragment>
+            <section className='container bg-dark'>
+                <section class="faq-section ">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6 offset-md-3">
+                                <div class="faq-title text-center pb-3">
+                                    <h2>FAQ</h2>
+                                </div>
+                            </div>
+                            <div class="col-md-6 offset-md-3">
+                                <div class="faq" id="accordion">
+                                    {FAQQuestion.map((item, index) =>
+                                        <div class="card" key={index} onClick={()=>toggleAccordion(index)}>
+                                            <div class="card-header" id="faqHeading-1">
+                                                <div class="mb-0">
+                                                    <h5 class="faq-title" data-toggle="collapse" data-target="#faqCollapse-1" data-aria-expanded="true" data-aria-controls="faqCollapse-1">
+                                                        <span class="badge">{index + 1}</span>{item.question}
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div className={accordion===index ? "active":"collap"}id="faqCollapse-1" aria-labelledby="faqHeading-1" data-parent="#accordion">
+                                                <div class="card-body">
+                                                    <p>{item.answer} </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </section>
+        </React.Fragment>
+    )
+}
+
+export default FAQ
---
+++ src/data/FAQQuestion.js
@@ -0,0 +1,22 @@
+export const FAQQuestion = [
+    {
+        "question": "What is photosynthesis?",
+        "answer": "Photosynthesis is the process by which plants and other organisms convert light energy into chemical energy, typically using carbon dioxide and water to produce glucose and oxygen. This vital process fuels the growth and sustenance of plants and contributes to the Earth's oxygen levels."
+    },
+    {
+        "question": "What is the significance of the water cycle?",
+        "answer": "The water cycle, also known as the hydrological cycle, is crucial for maintaining Earth's water balance. It involves processes like evaporation, condensation, and precipitation, ensuring the constant movement of water between the atmosphere, oceans, and land. This cycle sustains ecosystems, provides freshwater for various uses, and influences weather patterns."
+    },
+    {
+        "question": "How does the immune system work?",
+        "answer": "The immune system is the body's defense mechanism against harmful pathogens. It consists of cells, tissues, and organs that work together to identify and neutralize foreign invaders like viruses and bacteria. Immune responses include producing antibodies, activating immune cells, and developing immunological memory to provide long-term protection."
+    },
+    {
+        "question": "What is the theory of relativity?",
+        "answer": "The theory of relativity, formulated by Albert Einstein, comprises two main theories: special relativity and general relativity. Special relativity describes the physics of objects moving at constant velocity, while general relativity extends these concepts to include gravity's effect on spacetime. These theories have revolutionized our understanding of the universe's fundamental principles."
+    },
+    {
+        "question": "How does renewable energy impact the environment?",
+        "answer": "Renewable energy sources like solar, wind, and hydropower offer sustainable alternatives to fossil fuels. They generate electricity with lower greenhouse gas emissions, reducing air pollution and mitigating climate change. By minimizing dependence on non-renewable resources, renewable energy contributes to cleaner air, water, and a healthier planet for future generations."
+    },
+]
